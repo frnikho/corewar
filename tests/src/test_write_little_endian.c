ad4a18edcdb1a7424bf25ea1a2c0ae880dff7237
nicolas.sansd <nicolas.sansd@gmail.com>
2020-05-14 12:21:31 +0200

    created some unit tests

---
+++ tests/src/test_write_little_endian.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** write little endian
+** File description:
+** unit tests
+*/
+
+#include "corewar.h"
+#include "op.h"
+#include <criterion/criterion.h>
+
+Test(little_endian, e) {
+
+}
